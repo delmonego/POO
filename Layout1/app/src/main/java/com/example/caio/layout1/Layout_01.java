e866f9afa47e059777257a06680ff0d3b0997cee
delmonego <caiodelmonego@gmail.com>
2016-08-23 19:00:10 -0300

    Layout

---
+++ Layout1/app/src/main/java/com/example/caio/layout1/Layout_01.java
@@ -0,0 +1,13 @@
+package com.example.caio.layout1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layout_01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_01);
+    }
+}
